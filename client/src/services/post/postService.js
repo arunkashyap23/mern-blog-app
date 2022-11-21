7f875379453391c2491e4f267520910dbc500992
Arun <kashyapar002@gmail.com>
2022-11-21 15:28:51 +0530

    search by username

--- client/src/services/post/postService.js
+++ client/src/services/post/postService.js
@@ -7,7 +7,7 @@ export const postApi = createApi({
   endpoints: (builder) => ({
     getAllPost: builder.query({
       query: (query) => {
-        console.log(query);
+        // console.log(query);
         return {
           url: query ? query : "/",
           method: "GET",
